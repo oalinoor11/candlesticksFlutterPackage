ac31db2f7cea21338a83909308a2d983dd51d883
oalinoor11 <oalinoor11@gmail.com>
2024-01-24 09:09:30 +0600

    nn

--- lib/src/widgets/price_column.dart
+++ lib/src/widgets/price_column.dart
@@ -108,7 +108,7 @@ class _PriceColumnState extends State<PriceColumn> {
                     color: widget.lastCandle.isBull
                         ? widget.style.primaryBull
                         : widget.style.primaryBear,
-                    width: 3500,
+                    width: 5000,
                     height: 1,
                   ),
                   Container(
